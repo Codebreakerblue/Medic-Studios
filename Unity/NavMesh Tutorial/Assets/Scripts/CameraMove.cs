192fceca72e298bd468633608e92012e1ca77ff0
Codebreakerblue <thecodebreakerblue@gmail.com>
2020-12-17 14:05:26 -0700

    Created basic orbit camera, started basic click-to-go pathfinding

---
+++ Unity/NavMesh Tutorial/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public int Speed = 10;
+    public GameObject CameraTarget;
+
+    void Update()
+    {
+        
+
+    }
+
+    void CameraTargetControl()
+    {
+
+    }
+
+    void CameraClickDragControl()
+    {
+        //get input axes
+        float xAxisValue = Input.GetAxis("Horizontal") * Speed;
+        float zAxisValue = Input.GetAxis("Vertical") * Speed;
+
+        transform.position = new Vector3(transform.localPosition.x + xAxisValue, transform.localPosition.y, transform.localPosition.z + zAxisValue);
+    }
---
+++ Unity/NavMesh Tutorial/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour
+{
+    public GameObject CameraOrbitCenter;
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        float scrollFactor = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollFactor != 0)
+        {
+            CameraOrbitCenter.transform.localScale = CameraOrbitCenter.transform.localScale * (1f - scrollFactor);
+        }
+
+    }
+}
